5225fa04b2d3972fbfd875f4a7e798a1313d05a7
chengxueming <2694149918@qq.com>
2017-04-23 23:38:51 +0800

    try to desgin a game map for smbx resource

---
+++ editor/block.js
@@ -0,0 +1,16 @@
+{
+	4:{
+		"onBlock":function() {
+			// body...
+		},
+		"type":"special",
+	},
+	25,26,27,28:{
+		"collide":"top",
+		"type":"sizeAble"
+	},
+	31:{
+		"type":"pipe",
+		""
+	}
+}
---
+++ editor/enemy.js
@@ -0,0 +1,15 @@
+{
+	1:{
+		"move":function() {
+			// body...
+		}
+	},
+	25{
+		"collide":"top",
+		"type":"sizeAble"
+	},
+	31:{
+		"type":"pipe",
+		""
+	}
+}
